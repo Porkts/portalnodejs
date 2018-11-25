a53e7e30b6529f1a9dec6f03c06fad27a7ff72e5
Everton Recchi <porktss@gmail.com>
2018-11-25 14:41:41 -0200

    Usando o consign para carregar dinamicamente as rotas

--- config/server.js
+++ config/server.js
@@ -1,9 +1,12 @@
 var express = require('express')
 var app = express()
+var consign = require('consign')
 
 // setando configuracoes
 app.set('view engine', 'ejs')
 app.set('views', './app/views')
+
+consign().include('app/routes').into(app)
 
 // exportando o modulo
 module.exports = app
--- index.js
+++ index.js
@@ -5,12 +5,6 @@ var app = require('./config/server')
 var color = require('colors')
 
 
-// Rotas
-var rotaNoticias = require('./app/routes/noticias')(app)
-var rotaHome = require('./app/routes/home')(app)
-var rotaForm = require('./app/routes/formulario_inclusao_noticia')(app)
-
-
 app.listen(PORT, () => {
 	console.log('Servidor Rodando com Sucesso na porta '.green.bold.underline, PORT)
 })
